4e6db2e8cc07131cc2085d8b1c9926892fcc396c
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:12:31 +0530

    Configure S3 backend for Terraform state

--- terraform/infra-cluster-environment/backend.tf
+++ terraform/infra-cluster-environment/backend.tf
@@ -1,1 +1,10 @@
 
+terraform {
+  backend "s3" {
+    bucket         = "my-tf-state-bucket"   # change
+    key            = "eks/dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "tf-lock"
+    encrypt        = true
+  }
+}
