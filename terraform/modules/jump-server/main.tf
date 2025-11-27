e391558e35bf33bfe5d420c2e590d6b53f04ff98
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:17:53 +0530

    Add AWS security group and jump server instance

--- terraform/modules/jump-server/main.tf
+++ terraform/modules/jump-server/main.tf
@@ -1,1 +1,36 @@
 
+resource "aws_security_group" "jump" {
+  name        = "${var.env}-jump-sg"
+  description = "Allow SSH"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # tighten later
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jump" {
+  ami                    = "ami-0e742cca61fb65051" # change to latest in your region
+  instance_type          = var.instance_type
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [aws_security_group.jump.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "${var.env}-jump-server"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.jump.public_ip
+}
