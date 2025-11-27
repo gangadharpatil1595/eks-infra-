0c6d5c1301d813b0595b8e3ea1e3eeeca4f2bc0e
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:13:52 +0530

    Add outputs for cluster name, endpoint, and jump server IP

--- terraform/infra-cluster-environment/outputs.tf
+++ terraform/infra-cluster-environment/outputs.tf
@@ -1,1 +1,12 @@
 
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "jump_server_public_ip" {
+  value = module.jump_server.public_ip
+}
