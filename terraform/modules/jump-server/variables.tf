0edacbc980375cb60f89d5f4e66cf9d6f7379ef0
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:17:26 +0530

    Add variables for jump server module

--- terraform/modules/jump-server/variables.tf
+++ terraform/modules/jump-server/variables.tf
@@ -1,1 +1,6 @@
+variable "env"              { type = string }
+variable "vpc_id"           { type = string }
+variable "public_subnet_id" { type = string }
+variable "key_name"         { type = string }
+variable "instance_type"    { type = string }
 
