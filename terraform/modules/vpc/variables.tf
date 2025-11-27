3375a1512326f05d3fc2f528057c11d31faef654
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:14:43 +0530

    Add VPC variables for environment and subnets

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,1 +1,5 @@
 
+variable "env"            { type = string }
+variable "vpc_cidr"       { type = string }
+variable "public_subnets" { type = list(string) }
+variable "private_subnets"{ type = list(string) }
