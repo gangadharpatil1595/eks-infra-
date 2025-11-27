7e89b33e5dda602296d77f786505e616146f6dc2
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:16:47 +0530

    Add EKS cluster and IAM role resources

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,1 +1,37 @@
 
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {
+    subnet_ids = var.private_subnet_ids
+  }
+}
+
+resource "aws_iam_role" "cluster" {
+  name = "${var.env}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
