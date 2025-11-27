3647d14e3e4e70fda6845a1206bd89d1cdcf2a87
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:16:09 +0530

    Add variables for EKS module configuration

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,1 +1,5 @@
 
+variable "env"               { type = string }
+variable "cluster_name"      { type = string }
+variable "vpc_id"            { type = string }
+variable "private_subnet_ids"{ type = list(string) }
