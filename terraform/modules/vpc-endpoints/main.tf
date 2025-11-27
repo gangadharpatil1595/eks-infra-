a10961fdf7fdd70ac1686a8612174de1404c1a6c
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:19:22 +0530

    Add AWS VPC endpoint resource for S3

--- terraform/modules/vpc-endpoints/main.tf
+++ terraform/modules/vpc-endpoints/main.tf
@@ -1,1 +1,10 @@
 
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = var.vpc_id
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [] # add your private RTs if you create them
+}
+
+data "aws_region" "current" {}
