2dd5c91ee89c9e87d3537208e066d8edb76e28ef
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:15:10 +0530

    Add VPC and subnet resources in Terraform

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,1 +1,46 @@
 
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.env}-vpc"
+    Env  = var.env
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.env}-public-${count.index}"
+    Env  = var.env
+  }
+}
+
+resource "aws_subnet" "private" {
+  count      = length(var.private_subnets)
+  vpc_id     = aws_vpc.this.id
+  cidr_block = var.private_subnets[count.index]
+
+  tags = {
+    Name = "${var.env}-private-${count.index}"
+    Env  = var.env
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  value = [for s in aws_subnet.private : s.id]
+}
