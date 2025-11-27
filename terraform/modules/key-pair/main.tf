583b4f1b008f0f181c1dc60a0f7667dc3536526a
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:18:29 +0530

    Add variables and key pair resource in main.tf

--- terraform/modules/key-pair/main.tf
+++ terraform/modules/key-pair/main.tf
@@ -1,1 +1,12 @@
 
+variable "env"       { type = string }
+variable "public_key"{ type = string }
+
+resource "aws_key_pair" "this" {
+  key_name   = "${var.env}-jump-key"
+  public_key = var.public_key
+}
+
+output "key_name" {
+  value = aws_key_pair.this.key_name
+}
