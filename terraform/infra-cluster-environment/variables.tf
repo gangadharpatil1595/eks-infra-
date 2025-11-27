31078476f4e3b174156a517905d08a05bd8cc63e
gangadharpatil1595 <gangadharpatil1595@gmail.com>
2025-11-27 14:12:56 +0530

    Define variables for AWS infrastructure
    Added variables for AWS region, environment, VPC CIDR, subnets, cluster name, public key, and jump instance type.

--- terraform/infra-cluster-environment/variables.tf
+++ terraform/infra-cluster-environment/variables.tf
@@ -1,1 +1,36 @@
 
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "env" {
+  type        = string
+  description = "Environment name (dev/stage/prod)"
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "public_key" {
+  type        = string
+  description = "SSH public key for bastion/jump server"
+}
+
+variable "jump_instance_type" {
+  type    = string
+  default = "t3.micro"
+}
